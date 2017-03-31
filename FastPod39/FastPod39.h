6aec711c4a9c6461fbce7c689d28b02068b53f9a
JEONGIN HWANG <jeonginhwang@JEONGIN-HWANG-no-MacBook-Pro.local>
2017-03-31 11:13:34 +0900

    Initial commit :)

---
+++ FastPod39/FastPod39.h
@@ -0,0 +1,19 @@
+//
+//  FastPod39.h
+//  FastPod39
+//
+//  Created by JEONGIN HWANG on 2017/03/31.
+//  Copyright © 2017年 JEONGIN HWANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FastPod39.
+FOUNDATION_EXPORT double FastPod39VersionNumber;
+
+//! Project version string for FastPod39.
+FOUNDATION_EXPORT const unsigned char FastPod39VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FastPod39/PublicHeader.h>
+
+
